081e6bf12a84c3e77762d7f7bc13232e7668d630
cetherdev <robert.hct@gmail.com>
2024-10-18 00:22:08 -0600

    Implementación de Auth Repository

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
 
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+    const datasource = new AuthDatasourceImpl();
+    const authRepository = new AuthRepositoryImpl(datasource);
+    const controller = new AuthController(authRepository);
 
     router.post('/login', (req, res) => controller.loginUser(req, res))
     router.post('/register', (req, res) => controller.registerUser(req, res))
