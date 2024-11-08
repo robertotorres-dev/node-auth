132727efd07ebfcf216fe75f246749e4c5d9e5fc
cetherdev <robert.hct@gmail.com>
2024-11-07 21:09:53 -0600

    add data source and encript password

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -1,3 +1,5 @@
+import { BcryptAdapter } from "../../config";
+import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthDatasourceImpl implements AuthDatasource {
@@ -5,29 +7,31 @@ export class AuthDatasourceImpl implements AuthDatasource {
     const { email, password, name } = registerUsrDto;
 
     try {
+      const exists = await UserModel.findOne({ email });
+      if (exists) throw CustomError.badRequest('User already exists');
 
-      // 1. Verificar si existe
-
-      // 2. Hash contraseña
-
-      // 3. Mapear respuesta a entidad
-
-      return new UserEntity(
-        '1',
+      const user = await UserModel.create({
         name,
         email,
-        password,
-        ['ADMIN_ROLE']
+        password: BcryptAdapter.hash(password)
+      });
+
+      await user.save();
+
+      return new UserEntity(
+        user.id,
+        name,
+        email,
+        user.password,
+        user.roles
       );
 
     } catch (error) {
-
       if (error instanceof CustomError) {
         throw error;
       }
 
       throw CustomError.internalServerError()
-
     }
 
   }
